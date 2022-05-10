68820450484362e7aeb8b909c180f2023cb73fd6
MaiteSM <mai.sanmartin@gmail.com>
2022-05-09 22:57:43 -0300

    mas cositas

--- src/clustering/z461_cluster_jerarquico_m.R
+++ src/clustering/z461_cluster_jerarquico_m.R
@@ -22,9 +22,12 @@ setwd( "~/buckets/b1/" )  #cambiar por la carpeta local
 #leo el dataset
 dataset  <- fread( "./datasets/paquete_premium.csv.gz", stringsAsFactors= TRUE)
 
-#me quedo SOLO con los BAJA+2
+
 #dataset  <- dataset[  clase_ternaria =="BAJA+2" , ]
 dataset  <- dataset[  foto_mes>=202001  & foto_mes<=202011, ]
+dataset <- dataset [, uni := 0]
+dataset <- dataset [clase_ternaria == "CONTINUA", uni := runif(1)]
+dataset <- dataset %>% filter(uni < 0.3)
 gc()
 
 #quito los nulos para que se pueda ejecutar randomForest,  Dios que algoritmo prehistorico ...
@@ -33,20 +36,20 @@ dataset  <- na.roughfix( dataset )
 
 #los campos que arbitrariamente decido considerar para el clustering
 #por supuesto, se pueden cambiar
-# campos_buenos  <- c( "ctrx_quarter", "cpayroll_trx", "mcaja_ahorro", "mtarjeta_visa_consumo", "ctarjeta_visa_trx",
-#                      "mcuentas_saldo", "mrentabilidad_annual", "mprestamos_personales", "mactivos_margen", "mpayroll",
-#                      "Visa_mpagominimo", "Master_fechaalta", "cliente_edad", "chomebanking_trx", "Visa_msaldopesos",
-#                      "Visa_Fvencimiento", "mrentabilidad", "Visa_msaldototal", "Master_Fvencimiento", "mcuenta_corriente",
-#                      "Visa_mpagospesos", "Visa_fechaalta", "mcomisiones_mantenimiento", "Visa_mfinanciacion_limite",
-#                      "mtransferencias_recibidas", "cliente_antiguedad", "Visa_mconsumospesos", "Master_mfinanciacion_limite",
-#                      "mcaja_ahorro_dolares", "cproductos", "mcomisiones_otras", "thomebanking", "mcuenta_debitos_automaticos",
-#                      "mcomisiones", "Visa_cconsumos", "ccomisiones_otras", "Master_status", "mtransferencias_emitidas",
-#                      "mpagomiscuentas")
+campos_buenos  <- c( "ctrx_quarter", "cpayroll_trx", "mcaja_ahorro", "mtarjeta_visa_consumo", "ctarjeta_visa_trx",
+                     "mcuentas_saldo", "mrentabilidad_annual", "mprestamos_personales", "mactivos_margen", "mpayroll",
+                     "Visa_mpagominimo", "Master_fechaalta", "cliente_edad", "chomebanking_trx", "Visa_msaldopesos",
+                     "Visa_Fvencimiento", "mrentabilidad", "Visa_msaldototal", "Master_Fvencimiento", "mcuenta_corriente",
+                     "Visa_mpagospesos", "Visa_fechaalta", "mcomisiones_mantenimiento", "Visa_mfinanciacion_limite",
+                     "mtransferencias_recibidas", "cliente_antiguedad", "Visa_mconsumospesos", "Master_mfinanciacion_limite",
+                     "mcaja_ahorro_dolares", "cproductos", "mcomisiones_otras", "thomebanking", "mcuenta_debitos_automaticos",
+                     "mcomisiones", "Visa_cconsumos", "ccomisiones_otras", "Master_status", "mtransferencias_emitidas",
+                     "mpagomiscuentas")
 
 
 
 #Ahora, a esperar mucho con este algoritmo del pasado que NO correr en paralelo, patetico
-modelo  <- randomForest( x= dataset, #[  , campos_buenos, with=FALSE ], 
+modelo  <- randomForest( x= dataset[  , campos_buenos, with=FALSE ], 
                          y= NULL, 
                          ntree= 1000, #se puede aumentar a 10000
                          proximity= TRUE, 
